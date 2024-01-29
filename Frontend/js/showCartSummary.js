446748ade0e2c2c8acfdb75b353eef135c814c6f
SzRN23 <146065767+SzRN23@users.noreply.github.com>
2024-01-29 11:01:32 +0100

    Check Out Item törlés
    A Check Out lapon lévő kosár részhez csináltam egy funkciót, ami törli a kosárból az adott terméket ha annak mennyisége 0.

--- Frontend/js/showCartSummary.js
+++ Frontend/js/showCartSummary.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     showCartSummary();
 });
 
-function showCartSummary(){
+function showCartSummary() {
     var cartSummary = JSON.parse(localStorage.getItem("cart")) || [];
     var cartSummaryElement = document.getElementById("cartSummary");
 
     cartSummaryElement.innerHTML = "";
 
-    cartSummary.forEach(function (product) {
+    cartSummary.forEach(function (product, index) {
         var div = document.createElement("div");
         div.innerHTML = `<div class="row">
                             <div class="col-8">
@@ -20,7 +20,7 @@ function showCartSummary(){
                                         <p class="productName">${product.name} ${(product.size).toUpperCase()}</p>
                                     </div>
                                     <div class="col-3 align-self-center">
-                                        <input class="quantity" type="number" value="${product.quantity}" min="0">
+                                        <input class="quantity" type="number" value="${product.quantity}" min="0" onchange="removeFromCartSummary(${index}, this.value)">
                                     </div>
                                     <div class="col-3 align-self-center">
                                         <p class="price">${product.price} Ft</p>
@@ -30,4 +30,25 @@ function showCartSummary(){
                         </div>`;
         cartSummaryElement.appendChild(div);
     });
+}
+
+function removeFromCartSummary(index, newQuantity) {
+    var cartSummary = JSON.parse(localStorage.getItem("cart")) || [];
+
+    // index validálás
+    if (index >= 0 && index < cartSummary.length) {
+        // quantity frissítése
+        cartSummary[index].quantity = parseInt(newQuantity);
+
+        // item törlése, ha a quantity-ja 0
+        if (cartSummary[index].quantity === 0) {
+            cartSummary.splice(index, 1);
+        }
+
+        // cart frissítése localStorage-ban
+        localStorage.setItem("cart", JSON.stringify(cartSummary));
+
+        // A cartSummary frissítése
+        showCartSummary();
+    }
 }
